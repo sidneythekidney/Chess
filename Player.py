f7d91818492aeb1a48139f5817964dcd6a53d34f
sidmeade <sidmeade@umich.edu>
2020-03-27 10:27:25 -0400

    Created eval function headers

--- Player.py
+++ Player.py
@@ -28,14 +28,64 @@ class Player():
         while(len(self.enemy_pieces) > 0):
             del self.enemy_pieces[0]
 
-    # Makes the given move so we can evaluate it:
-    def make_pot_move(self, piece, move):
+    # Checkmate function:
+    def is_checkmate(self, piece, pieces, enemy_pieces):
+        # Check to see if we can be checkmated
+        # Check to see if we caused checkmate
         pass
 
-    # Checkmate function:
-    def is_checkmate(self, move, piece):
-        # Take in the move and the piece moving:
+    def gained_material(self, piece, pieces, enemy_pieces, curr_diff):
+        move_diff = self.calc_diff(pieces, enemy_pieces)
+        # Add in weight:
+        weight = 10
+        return weight * (move_diff - curr_diff)
+
+    def is_supported(self, piece, pieces, enemy_pieces):
+        # Check to see the support for each friendly piece
         pass
+
+    def is_attacked(self, piece, pieces, enemy_pieces):
+        # Check to see the attack options of each enemy piece
+        pass
+
+    def can_attack(self, piece, pieces, enemy_pieces):
+        # Check to see if there are any pieces we can attack.
+        pass
+
+    def win_exchange(self, piece, pieces, enemy_pieces):
+        # Check to see whether or not we win an exchange
+        pass
+
+    def square_concentration(self, piece, pieces, enemy_pieces):
+        # Check to see if we can brute force our way onto a square.
+        pass
+
+    def calc_diff(self, pieces, enemy_pieces):
+        adder = 0
+        for piece in pieces:
+            if piece.name == "Pawn":
+                adder += 1
+            if piece.name == "Bishop":
+                adder += 3
+            if piece.name == "Knight":
+                adder += 3
+            if piece.name == "Rook":
+                adder += 5
+            if piece.name == "Queen":
+                adder += 9
+        subtractor = 0
+        for piece in enemy_pieces:
+            if piece.name == "Pawn":
+                subtractor += 1
+            if piece.name == "Bishop":
+                subtractor += 3
+            if piece.name == "Knight":
+                subtractor += 3
+            if piece.name == "Rook":
+                subtractor += 5
+            if piece.name == "Queen":
+                subtractor += 9
+        return adder - subtractor
 
     # Perform evaluations for each potential turn:
     def evaluation(self):
@@ -54,15 +104,24 @@ class Player():
                 copy_pieces = copy.deepcopy(self.pieces)
                 copy_enemy_pieces = copy.deepcopy(self.enemy_pieces)
 
+                # Set a current diff measure to calculate diff score:
+                current_diff = self.calc_diff(copy_pieces, copy_enemy_pieces) 
+
                 # Make the move we need to evaluate:
                 piece.move_piece(self.tiles[piece.potential_moves[move][0]*8+piece.potential_moves[move][1]],
-                                self.tiles, copy_pieces, copy_enemy_pieces, self.gameDisplay, False, False)
+                                self.tiles, copy_pieces, copy_enemy_pieces, self.gameDisplay, True, True)
 
                 # Reset current move evaluation to 0.
                 current_move = 0
 
-                # Perform all evaluations
-                current_move += self.is_checkmate(move, piece)
+                # Perform all evaluations here
+                current_move += self.is_checkmate(piece, copy_pieces, copy_enemy_pieces)
+                current_move += self.gained_material(piece, copy_pieces, copy_enemy_pieces, current_diff)
+                current_move += self.is_supported(piece, copy_pieces, copy_enemy_pieces)
+                current_move += self.is_attacked(piece, copy_pieces, copy_enemy_pieces)
+                current_move += self.can_attack(piece, copy_pieces, copy_enemy_pieces)
+                current_move += self.win_exchange(piece, copy_pieces, copy_enemy_pieces)
+                current_move += self.square_concentration(piece, copy_pieces, copy_enemy_pieces)
 
                 # Check to see if the evaluation is the best move:
                 if current_move > best_move_value:
